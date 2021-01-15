2a152d3145f26c60b7c6c718231e38df05ba01b6
Hermann Harris <harris.hermann@gmail.com>
2021-01-15 15:08:08 +0200

    All tests passing & outputs proper html

--- lib/Parser.rb
+++ lib/Parser.rb
@@ -1,3 +1,6 @@
+require 'digest/md5'
+require 'nokogiri'
+
 class Parser
 
   attr_reader :text, :highlight_hashes
@@ -9,64 +12,90 @@ class Parser
 
 
   def call
-    construct_html
+    html = Nokogiri::HTML::DocumentFragment.parse(wrap_text).to_html
+    File.write('index.html', html)
   end
 
 
   private
 
-  def clean_text_array
-    text.split("\n\n").map(&:strip)
+  def generate_color(hash)
+    "##{Digest::MD5.hexdigest(hash.to_s)[0..5]}"
+  end
+
+  def paint_highlight_hashes
+    highlight_hashes.map {|h|
+      { start: h[:start], end: h[:end], comment: h[:comment], color: generate_color(h)}
+    }
   end
 
   def sanitized_string
-    text.split("\n\n").map(&:strip).join(' \n\n ')
+    text.strip.gsub(/[ ]{2,}/, '')
   end
 
-  def words_array
-    sanitized_string.gsub(' \\n\\n ', ' ').split
+  def entities
+    sanitized_string.lines.map(&:chomp).map { |x| x.empty? ? "" : x.split(/(?<=[\s])/) }.flatten
   end
 
-  def line_break_index
-    sanitized_string.split.each_with_index.map { |item, index| index if item == '\\n\\n' }.compact
-  end
-
-  def sort_highlight_hashes
-    highlight_hashes.sort_by { |hsh| hsh[:start] }
+  def line_breaks
+    entities.each_with_index.map { |item, index| index if item.empty? }.compact
   end
 
   def apply_highlight_tags
-    words = words_array
-    sort_highlight_hashes.each do |highlight|
-      starter = highlight[:start]
-      ender = highlight[:end] - 1
-      words[starter] = '<mark>' + words[starter]
-      words[ender] = words[ender] + '</mark>'
-    end
+    words = entities.select { |entity| entity != "" }
+    paint_highlight_hashes.sort_by { |h| h[:start] }.map { |rule|
+      inclusive_line_breaks = line_breaks.select {|z| z.between?(rule[:start], rule[:end])}.sort
+      closing_index = (rule[:end] <= rule[:start]) ? rule[:start] : rule[:end] - 1
+      # Spans line breaks
+      unless inclusive_line_breaks.empty?
+        inclusive_line_breaks.each do |ln|
+          if ln == inclusive_line_breaks.first
+            # Add closing before
+            # Add opening on start
+            words[rule[:start]] = "<mark title='#{rule[:comment]}' style='background-color: #{rule[:color]};'>" + words[rule[:start]]
+            words[ln - 1] = words[ln - 1] + "</mark>"
+          else
+            # Add closing before
+            # Add opening after
+            words[ln] = "<mark title='#{rule[:comment]}' style='background-color: #{rule[:color]};'>" + words[ln]
+            words[ln - 1] = words[ln - 1] + "</mark>"
+          end
+
+          # IF LAST LINE BREAK USE END ELSE ADD BEFORE LINE BREAK
+          if ln == inclusive_line_breaks.last
+            # Add closing on end
+            # Add opening after
+
+            words[ln] = "<mark title='#{rule[:comment]}' style='background-color: #{rule[:color]};'>" + words[ln]
+            words[closing_index] = words[closing_index] + "</mark>"
+          else
+            # Add closing before
+            # Add opening after
+            words[ln] = "<mark title='#{rule[:comment]}' style='background-color: #{rule[:color]};'>" + words[ln]
+            words[ln - 1] = words[ln - 1] + "</mark>"
+          end
+        end
+      else
+        words[rule[:start]] = "<mark title='#{rule[:comment]}' style='background-color: #{rule[:color]};'>" + words[rule[:start]]
+        words[closing_index] = words[closing_index] + "</mark>"
+      end
+
+    }
     words
   end
 
-  def apply_line_breaks
-    with_highlights = apply_highlight_tags
-    line_break_index.each do |ln|
-      with_highlights.insert(ln, '/n/n')
+  def full_text_array
+    output = apply_highlight_tags
+    line_breaks.each do |line|
+      output.insert(line, "</p><p>")
     end
-    with_highlights
+    output
   end
 
-  def construct_html
-    lines = apply_line_breaks.join.split(line_break)
-    lines.map { |line| "<p>#{line}</p>" }.join
+  def wrap_text
+    "<p>#{full_text_array.join}</p>"
   end
 
-  def line_break
-    '/n/n'
-  end
 
-  # def apply_paragraph_tags
-  #   binding.pry
-  #   text = line_break_index.map  { |ln|  with_highlights.insert(index, '</p><p>') }
-  #   "<p>#{text}</p>"
-  # end
 
 end
--- spec/features/parser_spec.rb
+++ spec/features/parser_spec.rb
@@ -1,25 +1,35 @@
 require 'spec_helper'
 require 'parser'
 
+# Remove leading & trailing whitespace
+# 1 - prepend text with <p>
+# 1 - Append text with </p>
+# 2 - Replace all line breaks with '</p><<p>'
+# 3 - add highlight tags from array of words where p tags removed
+
+
 RSpec.describe Parser, type: :request do
 
   subject { described_class.new(text: text, highlight_hashes: highlight_hashes) }
-  let(:text) { %q{
-    Line One
+  let(:text) { "Line One
 
-    Line Two
-  }}
+    Line Two"}
 
   let(:highlight_hashes) { [
       {
         start: 3,
-        end: 4,
+        end: 3,
         comment: 'I highlight the word "Two" in sentence two'
       },
       {
         start: 0,
         end: 1,
         comment: 'I highlight the word "Line" in sentence one'
+      },
+      {
+        start: 0,
+        end: 3,
+        comment: 'I overlap'
       }
     ]
   }
@@ -34,48 +44,74 @@ RSpec.describe Parser, type: :request do
     end
 
     it 'sets highlight_hashes to the second argument' do
-      binding.pry
       expect(subject.highlight_hashes).to eq(highlight_hashes)
     end
   end
 
-  describe ".call" do
-    it { expect(subject.call).to eq(output) }
+  describe "#generate_color" do
+    it { expect(subject.send(:generate_color, highlight_hashes.first)).to eq('#1d85e1') }
+    it { expect(subject.send(:generate_color, highlight_hashes.last)).to eq('#22045b') }
+  end
+
+  describe "#paint_highlight_hashes" do
+    it { expect(subject.send(:paint_highlight_hashes)).to eq(painted_highlight_hashes) }
   end
 
   describe "#sanitized_string" do
-    it { expect(subject.send(:sanitized_string)).to eq("Line One \\n\\n Line Two") }
+    it { expect(subject.send(:sanitized_string)).to eq("Line One\n\nLine Two") }
   end
 
-  describe "#words_array" do
-    it { expect(subject.send(:words_array).count).to eq(4) }
+  describe "#line_breaks" do
+    it { expect(subject.send(:line_breaks)).to eq([2]) }
   end
 
-  describe "#line_break_index" do
-    it { expect(subject.send(:line_break_index)).to eq([2]) }
-  end
-
-  describe "#sort_highlight_hashes" do
-    it { expect(subject.send(:sort_highlight_hashes).first[:start]).to eq(0) }
-    it { expect(subject.send(:sort_highlight_hashes).last[:start]).to eq(3) }
+  describe "#entities" do
+    it { expect(subject.send(:entities)).to eq(["Line ", "One", "", "Line ", "Two"]) }
   end
 
   describe "#apply_highlight_tags" do
-    it { expect(subject.send(:apply_highlight_tags)).to eq(['<mark>Line</mark>', 'One', 'Line', '<mark>Two</mark>']) }
+    it { expect(subject.send(:apply_highlight_tags)).to eq(highlights_array) }
   end
 
-  describe "#apply_line_breaks" do
-    it { expect(subject.send(:apply_line_breaks)).to eq(['<mark>Line</mark>', 'One', '/n/n', 'Line', '<mark>Two</mark>']) }
+  describe "#full_text_array" do
+    it { expect(subject.send(:full_text_array)).to eq(full_text_array) }
   end
 
-  describe "#construct_html" do
-    it { expect(subject.send(:construct_html)).to eq(output) }
+  describe "#wrap_text" do
+    it { expect(subject.send(:wrap_text)).to eq(html_output) }
+  end
+
+  describe ".call" do
+    it { expect(subject.call).to be_a_kind_of(Integer) }
   end
 
   private
 
-  def output
-    "<p><mark>Line</mark> One</p><p>Line <mark>Two</mark></p>"
+  def painted_highlight_hashes
+    highlight_hashes.map {|h|
+      { start: h[:start], end: h[:end], comment: h[:comment], color: subject.send(:generate_color, h)}
+    }
+  end
+
+  def highlights_array
+    [
+      "<mark title='I overlap' style='background-color: #22045b;'><mark title='I highlight the word \"Line\" in sentence one' style='background-color: #ec561a;'>Line </mark>",
+      "One</mark>",
+      "<mark title='I overlap' style='background-color: #22045b;'>Line </mark>",
+      "<mark title='I highlight the word \"Two\" in sentence two' style='background-color: #1d85e1;'>Two</mark>"
+    ]
+  end
+
+  def full_text_array
+    output = highlights_array
+    subject.send(:line_breaks).each do |line|
+      output.insert(line, "</p><p>")
+    end
+    output
+  end
+
+  def html_output
+    "<p><mark title='I overlap' style='background-color: #22045b;'><mark title='I highlight the word \"Line\" in sentence one' style='background-color: #ec561a;'>Line </mark>One</mark></p><p><mark title='I overlap' style='background-color: #22045b;'>Line </mark><mark title='I highlight the word \"Two\" in sentence two' style='background-color: #1d85e1;'>Two</mark></p>"
   end
 
 end
